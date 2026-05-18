363ed5d2b68cbd6dc7c0a8a5a0d6f60bde2da51b
jaykel1jaykel <jaykelmirandaa@gmail.com>
2026-05-17 18:55:44 -0600

    validaciones
    Validaciones para la función principal de insertar pacientes

--- tkinder.py
+++ tkinder.py
@@ -5,59 +5,101 @@ import re
 ventana = Tk()
 ventana.title("Banco de Sangre")
 ventana.geometry("700x600")
-ventana.config(bg="#ffffff")
-ventana.resizable(False,False)
+ventana.config(bg="#f4f7fb")
+ventana.resizable(True,False)
 # ---------------- TITULO ----------------
 titulo = Label(ventana,text="DONAR SANGRE, ES DONAR VIDA",
     font=("Arial",24,"bold"),bg="#ffffff",fg="black")
 titulo.pack(pady=30)
 # ---------------- SUBTITULO ----------------
 subtitulo = Label(ventana,text="Sistema de Información - Banco de Sangre",
-    font=("Arial",14),bg="#ffffff",fg="black")
+    font=("Arial",14),bg="white",fg="black")
 subtitulo.pack(pady=5)
 # ---------------- MARCO DEL MENU ----------------
-frame_menu = Frame(ventana,bg="white",bd=4,relief="ridge")
-frame_menu.pack(pady=40)
+frameMenu = Frame(ventana,bg="#ffffff",bd=4,relief="ridge")
+frameMenu.pack(pady=40)
 # Inicializaciones 
 donantes = []
 # ---------------- FUNCIONES ----------------
 # Funcion Inseratar Donantes
 
 def cedulaInser(frameInsertar):
-    Label(frameInsertar, text = "Cedula", font=("Arial",14,"bold")).grid(row= 0,column=0)
+    Label(frameInsertar, text = "Cedula", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 0,column=0,pady=5)
     cedula = StringVar()
     Entry(frameInsertar,textvariable=cedula).grid(row=0,column=1)
-    cedula = cedula.get()
     return cedula
 
-def cedulaInserAux(frameInsertar):
-    cedula = cedulaInser(frameInsertar)
-    cedula = cedula.replace("-","")
-    formato = r"[1-9]{1}-[1-9]{4}-[1-9]{4}"
-    for i in cedula:
-        if i.isdigit() == False:
-            return (False,"Los datos ingresados nada mas deben ser numneros")
-    if int(cedula[0]) != 0:
-        return (False,"El primer dato de la cedula debe ser diferente a 0")
-    elif formato == cedula:
-        return (True,cedula)
-    else:
-        return (False,"Formato invalido ejemplo formato correcto #-####-####")
+def cedulaInserAux(cedula):
+    formato=r"[1-9]{1}-[0-9]{4}-[0-9]{4}"
+    numeros=cedula.replace("-","")
+    if numeros=="":
+        return(False,"Debe ingresar una cedula")
+    for i in numeros:
+        if i.isdigit()==False:
+            return(False,"Los datos ingresados in cedula nada mas deben ser numeros")
+    if re.fullmatch(formato,cedula) is None:
+        return(False,"Formato invalido en cedula, ejemplo de formato correcto #-####-####")
+    return(True,cedula)
     
 def nombreInser(frameInsertar):
-    Label(frameInsertar, text = "Nombre completo", font=("Arial",14,"bold")).grid(row= 1,column=0)
+    Label(frameInsertar, text = "Nombre completo", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 1,column=0,pady=5)
     nombre = StringVar()
     Entry(frameInsertar,textvariable=nombre).grid(row=1,column=1)
-    return nombre
+    return nombre   
+
+def normalizarNombre(pnombre):
+    if pnombre == "":
+        return (False,"Debe incuir un nombre")
+    for letra in pnombre:
+        if letra.isalpha() == False and letra != " ":
+            return(False,"Debe ingresar solamente letras en el nombre")
+    palabras = pnombre.split()     
+    resultado = ""
+    for p in palabras:                           
+        palabra = p[0].upper() + p[1:].lower()   
+        resultado = resultado + palabra + " "     
+    return (True,resultado)
 
 def fechaNacimientoInser(frameInsertar):
-    Label(frameInsertar, text = "Fecha de nacimiento", font=("Arial",14,"bold")).grid(row= 2,column=0)
+    Label(frameInsertar, text = "Fecha de nacimiento", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 2,column=0,pady=5)
     fechaNacimiento = StringVar()
     Entry(frameInsertar,textvariable= fechaNacimiento).grid(row=2,column=1)
     return fechaNacimiento
 
+def validarFechaAux(fechaNacimiento):
+    formato=r"\d{2}/\d{2}/\d{4}"
+    if re.fullmatch(formato,fechaNacimiento)is None :
+        return(False,"Formato invalido en fecha de nacimiento use el formato DD/MM/AAAA")
+    fecha=fechaNacimiento.split("/")
+    dia=int(fecha[0])
+    mes=int(fecha[1])
+    anno=int(fecha[2])
+    if mes<1 or mes>12:
+        return(False,"El mes ingresado en fecha de nacimiento no existe")
+    if anno<1900:
+        return(False,"El año ingresado en fecha de nacimiento no es valido")
+    if mes in [1,3,5,7,8,10,12]:
+        if dia<1 or dia>31:
+            return(False,"Ese mes ingredago en fecha de nacimiento solo tiene 31 dias")
+    elif mes in [4,6,9,11]:
+        if dia<1 or dia>30:
+            return(False,"Ese mes ingresado en fecha de nacimiento solo tiene 30 dias")
+    elif mes==2:
+        bisiesto=False
+        if anno%4==0 and anno%100!=0:
+            bisiesto=True
+        elif anno%400==0:
+            bisiesto=True
+        if bisiesto==True:
+            if dia<1 or dia>29:
+                return(False,"Fecha de nacimiento:  febrero en año bisiesto solo tiene 29 dias")
+        else:
+            if dia<1 or dia>28:
+                return(False,"Fecha de nacimiento: febrero solo tiene 28 dias")
+    return(True,fechaNacimiento)
+
 def tipoSangreInser(frameInsertar):
-    Label(frameInsertar, text = "Tipo de sangre", font=("Arial",14,"bold")).grid(row= 3,column=0)
+    Label(frameInsertar, text = "Tipo de sangre", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 3,column=0,pady=5)
     tipoSangre = StringVar()
     ttk.Combobox(frameInsertar,textvariable=tipoSangre,
         values=["O+","O-","A+","A-","B+","B-","AB+","AB-"]
@@ -65,36 +107,191 @@ def tipoSangreInser(frameInsertar):
     return tipoSangre
 
 def sexoInser(frameInsertar):
-    frameSexo = Frame(frameInsertar, bg = "white", bd = 5)
-    Label(frameSexo, text = "Sexo", font=("Arial",14,"bold")).grid(row= 0,column=0)
+    frameSexo = Frame(frameInsertar, bg = "#ffffff", bd = 5)
+    Label(frameSexo, text = "Sexo", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 0,column=0,pady=5)
     sexo = BooleanVar()
     Radiobutton(frameSexo, text= "Masculino",variable=sexo,value = True).grid(row=0,column=1)
-    Radiobutton(frameSexo, text= "Femenino",variable=sexo,value = False).grid(row=1,column=1)
+    Radiobutton(frameSexo, text= "Femenino",variable=sexo,value = False).grid(row=0,column=2)
     frameSexo.grid(row=4)
     return sexo
 
 def pesoInser(frameInsertar):
-    Label(frameInsertar, text = "Peso(kg)", font=("Arial",14,"bold")).grid(row= 5,column=0)
+    Label(frameInsertar, text = "Peso(kg)", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 5,column=0,pady=5)
     peso = StringVar()
     Entry(frameInsertar,textvariable=peso).grid(row=5,column=1)
     return peso
 
+def validarPesoAux(peso):
+    if peso.isdigit()==False:
+        return(False,"El peso solo debe contener numeros")
+    peso=int(peso)
+    if peso<50:
+        return(False,"El peso minimo permitido es 50 kg")
+    if peso>120:
+        return(False,"El peso maximo permitido es 120 kg")
+    return(True,peso)
+
 def telefonoInser(frameInsertar):
-    Label(frameInsertar, text = "Telefono", font=("Arial",14,"bold")).grid(row= 6,column=0)
+    Label(frameInsertar, text = "Telefono", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 6,column=0,pady=5)
     telefono = StringVar()
     Entry(frameInsertar,textvariable= telefono).grid(row=6,column=1)
     return telefono
 
+def validarTelefonoAux(telefono):
+    formato=r"[246789]{1}-?[0-9]{4}-?[0-9]{4}"
+    if re.fullmatch(formato,telefono)==False:
+        return(False,"Formato invalido en el numero de telefono, ejemplo correcto de formato ####-####")
+    telefono=telefono.replace("-","")
+    if telefono.isdigit()==False:
+        return(False,"El numero de telefono solo debe contener numeros y un '-' como separador.")
+    if telefono[0] in ["0","1","3","5"]:
+        return(False,"El numero telefonico no puede iniciar con 0,1,3 o 5")
+    return(True,telefono)
+
 def correoInser(frameInsertar):
-    Label(frameInsertar, text = "Correo", font=("Arial",14,"bold")).grid(row= 7,column=0)
+    Label(frameInsertar, text = "Correo", font=("Arial",14,"bold"),fg="#7A1027").grid(row= 7,column=0,pady=5)
     correo = StringVar()
     Entry(frameInsertar,textvariable=correo).grid(row=7,column=1)
     return correo
 
-def registrar(donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo):
-    donantes.append([cedula.get(),nombre.get(),fechaNacimiento.get(),tipoSangre.get(),
-        sexo.get(),peso.get(),telefono.get(),correo.get() ])
-    print(donantes)
+def validarCorreoAux(correo):
+    formato=r"[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com|yahoo\.com|outlook\.com|icloud\.com)$"
+    if re.fullmatch(formato,correo)==False:
+        return(False,"Correo invalido ejemplo formato de correo correcto ejemplo@gmail.com")
+    return(True,correo)
+#==============================Comentario despues de registrar los datos del donador===========================================
+
+def definirProvincia(n):
+    n = int(n)
+    if n==1:
+        return "San Jose"
+    elif n==2:
+        return "Alajuela"
+    elif n==3:
+        return "Cartago"
+    elif n == 4:
+        return "Heredia"
+    elif n == 5:
+        return "Guanacaste"
+    elif n == 6:
+        return "Puntarenas"
+    elif n == 7:
+        return "Limon"
+
+def calcularEdad(fechaNacimiento):
+    fecha=fechaNacimiento.split("/")
+    diaNacimiento=int(fecha[0])
+    mesNacimiento=int(fecha[1])
+    annoNacimiento=int(fecha[2])
+    diaActual=17
+    mesActual=5
+    annoActual=2026
+    edad=annoActual-annoNacimiento
+    if mesNacimiento>mesActual:
+        edad=edad-1
+    elif mesNacimiento==mesActual:
+        if diaNacimiento>diaActual:
+            edad=edad-1
+    return edad
+
+def mayorEdad(fechaNacimiento):
+    if calcularEdad(fechaNacimiento) >= 18:
+        return "Dado su fecha de nacimineto usted ya puede ser donador"
+    return"Dado su fecha de nacimiento usted aun no piede ser donador"
+
+def compatibilidadSangre(tipoSangre):
+    compatibilidad = {"O-":["O+","O-","A+","A-","B-","B+","AB-","AB+"],
+               "O+":["O+","A+","B+","AB+"],
+               "A-":["A+","A-","AB-","AB+"],
+               "A+":["A+","AB+"],
+               "B-":["B-","B+","AB-","AB+"],
+               "B+":["B+","AB+"],
+               "AB-":["AB-","AB+"],
+               "AB+":["AB+"]}
+    return compatibilidad[tipoSangre]
+
+def recomendacionSangre(tipoSangre):
+    if tipoSangre in ["A+","A-"]:
+        return "Se le recomienda ver este video sobre las particularidades de la sangre tipo 'A'"
+    return None
+
+def validarLugarNacimiento(cedula):
+    cedula = cedula.split("-")
+    provincia = definirProvincia(cedula[0])
+    lugaresDonacion={
+    "San Jose":["Banco Nacional de Sangre","Hospital Mexico","Hospital San Juan de Dios"],
+    "Alajuela":["Hospital San Rafael de Alajuela","Hospital de San Ramon","Hospital del Canton Norteno"],
+    "Cartago":["Hospital Max Peralta"],
+    "Heredia":["Hospital San Vicente de Paul"],
+    "Guanacaste":["Hospital La Anexion en Nicoya","Hospital Enrique Baltodano de Liberia"],
+    "Puntarenas":["Hospital Monsenor Sanabria"],
+    "Limon":["Hospital Tony Facio","Hospital de Guapiles"]}
+    ubicacion = str(lugaresDonacion[provincia])
+    return "Dado que usted nació en la provincia de:"+provincia+"usted podria donar en:"+ubicacion
+def generarAnalisisDonante(cedula,fechaNacimiento,tipoSangre,peso):
+    resultadoProvincia=validarLugarNacimiento(cedula)
+    edad=mayorEdad(fechaNacimiento)
+    compatibilidad=compatibilidadSangre(tipoSangre)
+    recomendacion=recomendacionSangre(tipoSangre)
+    mensaje=""
+    mensaje+=edad+"\n"
+    mensaje+= resultadoProvincia+"\n"
+    if int(peso)>=50 and int(peso):
+        mensaje+="usted posee un peso adecuado para donar sangre.\n"
+    elif int(peso) >120:
+        mensaje += "No puede donar sangre por sobrepeso\n"
+    else:
+        mensaje+="No puede donar sangre por bajo peso\n"
+    mensaje+="\nPuede donar a:\n"
+    for i in compatibilidad:
+        mensaje+=i+"\n"
+    if recomendacion != None:
+        mensaje+="\nRecomendacion:\n"
+        mensaje+=recomendacion
+    return mensaje
+
+def validarDatos(donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo):
+    resultadoCedula=cedulaInserAux(cedula)
+    if resultadoCedula[0]==False:
+        return resultadoCedula
+    resultadoNombre = normalizarNombre(nombre)
+    if resultadoNombre[0] == False:
+        return resultadoNombre
+    resultadoFecha=validarFechaAux(fechaNacimiento)
+    if resultadoFecha[0]==False:
+        return resultadoFecha
+    resultadoPeso=validarPesoAux(peso)
+    if resultadoPeso[0]==False:
+        return resultadoPeso
+    resultadoTelefono=validarTelefonoAux(telefono)
+    if resultadoTelefono[0]==False:
+        return resultadoTelefono
+    resultadoCorreo=validarCorreoAux(correo)
+    if resultadoCorreo[0]==False:
+        return resultadoCorreo
+    donantes.append([cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo]) # Agregar una validacion para que no se repitan datos
+    return(True,"Donante registrado correctamente")
+
+def registrar(ventanaInsertar,donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo,mensaje):
+    mensajeRegistrar = Label(ventanaInsertar,text = "")
+    resultado=validarDatos(
+        donantes,
+        cedula.get(),
+        nombre.get(),
+        fechaNacimiento.get(),
+        tipoSangre.get(),
+        sexo.get(),
+        peso.get(),
+        telefono.get(),
+        correo.get())
+    if resultado[0]==False:
+        mensaje.config(text=resultado[1],fg="red")
+        return
+    mensaje.config(text=resultado[1],fg="green")
+    mensajeRegistrar.config(text= generarAnalisisDonante(cedula.get(),fechaNacimiento.get(),tipoSangre.get(),peso.get()),font=("calibri",11,"bold"))
+    mensajeRegistrar.pack()
+
+
 def limpiar(cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo):
     cedula.set("")
     nombre.set("")
@@ -104,32 +301,32 @@ def limpiar(cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo):
     peso.set("")
     telefono.set("")
     correo.set("")
+
 def salirInser(ventanaInsertar):
     ventanaInsertar.destroy()
-def crearBotones(frameBotones,ventanaInsertar,donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo):
+
+def crearBotones(frameBotones,ventanaInsertar,mensaje,donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo):
     Button(frameBotones,text="Registrar",font=("Arial",12,"bold"),
-        bg="#97e6df",fg="black",
-        command=lambda: registrar(donantes,cedula,nombre,fechaNacimiento,
-            tipoSangre,sexo,peso,telefono,correo)
-    ).grid(row=0,column=0)
+        bg="#8B1E3F",fg="white",
+        command=lambda: registrar(ventanaInsertar,donantes,cedula,nombre,fechaNacimiento,
+            tipoSangre,sexo,peso,telefono,correo,mensaje)
+    ).grid(row=0,column=0,padx=10)
     Button(frameBotones,text="Limpiar",
-        font=("Arial",12,"bold"),bg="#97e6df",fg="black",
+        font=("Arial",12,"bold"),bg="#8B1E3F",fg="white",
         command=lambda: limpiar(cedula,nombre,fechaNacimiento,
-            tipoSangre,sexo,peso,telefono,correo)
-            ).grid(row=0,column=1)
-    Button(frameBotones,
-        text="Salir",
-        font=("Arial",12,"bold"),
-        bg="#97e6df",
-        fg="black",
+        tipoSangre,sexo,peso,telefono,correo)
+        ).grid(row=0,column=1,padx=10)
+    Button(frameBotones,text="Regresar",font=("Arial",12,"bold"),
+        bg="#2E2E2E",fg="white",
         command=lambda: salirInser(ventanaInsertar)
-    ).grid(row=0,column=2)
+    ).grid(row=0,column=2,padx = 10)
+
 def insertarDonador(donantes):
     ventanaInsertar=Toplevel()
     ventanaInsertar.title("Insertar donador")
-    ventanaInsertar.geometry("700x600")
-    ventanaInsertar.config(bg="#F0BEBE")
-    frameInsertar=Frame(ventanaInsertar,bg="#D8FFF5",bd=5)
+    ventanaInsertar.geometry("700x900")
+    ventanaInsertar.config(bg="#F5F5F5")
+    frameInsertar=Frame(ventanaInsertar,bg="#FFFFFF",bd=5)
     frameInsertar.pack(pady=20)
     cedula=cedulaInser(frameInsertar)
     nombre=nombreInser(frameInsertar)
@@ -139,9 +336,11 @@ def insertarDonador(donantes):
     peso=pesoInser(frameInsertar)
     telefono=telefonoInser(frameInsertar)
     correo=correoInser(frameInsertar)
-    frameBotones=Frame(ventanaInsertar,bg="#DEFDFF",bd=5)
+    mensaje = Label(ventanaInsertar,text = "",font= ("arial",12,"bold"), bg = "white",fg= "red")
+    mensaje.pack(pady=10)
+    frameBotones=Frame(ventanaInsertar,bg="#FFFFFF",bd=5)
     frameBotones.pack(pady=20)
-    crearBotones(frameBotones,ventanaInsertar,donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo)
+    crearBotones(frameBotones,ventanaInsertar,mensaje,donantes,cedula,nombre,fechaNacimiento,tipoSangre,sexo,peso,telefono,correo)
 
 def generar():
     print("Generar donadores")
@@ -162,25 +361,25 @@ def salir():
     ventana.destroy()
 
 # ---------------- BOTONES ----------------
-Button(frame_menu,text="1. Insertar donador",font=("Arial",14),
-    width=30,height=2,bg="#77c1ec",fg="white",
+Button(frameMenu,text="1. Insertar donador",font=("Arial",14),
+    width=30,height=2,bg="#1565c0",fg="white",
     command=lambda:insertarDonador(donantes)).grid(row=1,column=0)
-Button(frame_menu,text="2. Generar donadores",font=("Arial",14),
-    width=30,height=2,bg="#77c1ec",fg="white",command=generar).grid(row=1,column=1)
-Button(frame_menu,text="3. Actualizar datos",font=("Arial",14),
-    width=30,height=2,bg="#77c1ec",fg="white",command=actualizar).grid(row=2,column=0)
-Button(frame_menu,text="4. Eliminar donador",font=("Arial",14),
-    width=30,height=2,bg="#77c1ec",fg="white",
+Button(frameMenu,text="2. Generar donadores",font=("Arial",14),
+    width=30,height=2,bg="#1565c0",fg="white",command=generar).grid(row=1,column=1)
+Button(frameMenu,text="3. Actualizar datos",font=("Arial",14),
+    width=30,height=2,bg="#1565c0",fg="white",command=actualizar).grid(row=2,column=0)
+Button(frameMenu,text="4. Eliminar donador",font=("Arial",14),
+    width=30,height=2,bg="#1565c0",fg="white",
     command=eliminar).grid(row=2,column=1)
-Button(frame_menu,text="5. Insertar lugar",font=("Arial",14),
-    width=30,height=2,bg="#77c1ec",fg="white",command=lugares).grid(row=3,column=0)
-Button(frame_menu,text="6. Reportes",font=("Arial",14),width=30,
-    height=2,bg="#77c1ec",fg="white",command=lambda:reportes(donantes)).grid(row=3,column=1)
-Button(frame_menu,text="7. Salir",font=("Arial",14),width=30,
-    height=2,bg="#62D885",fg="white",command=salir).grid(row=4)
+Button(frameMenu,text="5. Insertar lugar",font=("Arial",14),
+    width=30,height=2,bg="#1565c0",fg="white",command=lugares).grid(row=3,column=0)
+Button(frameMenu,text="6. Reportes",font=("Arial",14),width=30,
+    height=2,bg="#1565c0",fg="white",command=lambda:reportes(donantes)).grid(row=3,column=1)
+Button(frameMenu,text="7. Salir",font=("Arial",14),width=30,
+    height=2,bg="#2e7d32",fg="white",command=salir).grid(row=4,column=0,columnspan=2)
 # ---------------- FOOTER ----------------
 footer = Label(ventana,text="TEC - Taller de Programación",
-    font=("Arial black",12),bg="#ffffff",fg="black")
-footer.pack(side="bottom",pady=10)
+    font=("Arial black",12),bg="#1f255c",fg="white")
+footer.pack(side="bottom",pady=20, padx= 20)
 # ---------------- EJECUTAR ----------------
 ventana.mainloop()
